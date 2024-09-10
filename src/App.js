6861a0093ca34b0563b684ce6770739bba703cdc
JØƵ <jos@tharonline.nl>
2024-09-10 16:05:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import Logo from './components/Logo'
+import Form from './components/Form'
+import PackingList from './components/PackingList'
+import Stats from './components/Stats'
+
+export default function App () {
+  const [items, setItems] = useState([])
+
+  const handleAddItems = (item) => {
+    setItems(items => [...items, item])
+  }
+
+  const handleDeleteItem = (id) => {
+    setItems(items => items.filter(item => item.id !== id))
+  }
+
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item))
+  }
+
+  const handleClearList = () => {
+    const confirmed = window.confirm('Are you sure you want to clear the list?')
+
+    if (confirmed) setItems([])
+  }
+
+  return (<div className={'app'}>
+    <Logo/>
+    <Form onAddItems={handleAddItems}/>
+    <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearList={handleClearList} />
+    <Stats items={items}/>
+  </div>)
+
+}
